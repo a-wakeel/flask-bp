ba0517a37ae81c0ccbe12e6a91c13bad1f5cc9a9
Abdul Wakeel <a-wakeel@users.noreply.github.com>
2019-09-16 15:01:23 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 """
 flask-bp module to manage database migrations.
 """
-from flask_script import Manager  # pylint: disable=W0402
+from flask_script import Manager  # pylint: disable=W0402,R0401
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app, db
+from app import app, db  # pylint: disable=R0401
 
 migrate = Migrate(app, db)
 manager = Manager(app)
